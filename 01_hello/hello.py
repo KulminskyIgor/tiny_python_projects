b2231fc6eb21a6b801c6b807fd64bec1d596c892
Igor Kulminskii <igor.kulminskii@telekom.com>
2024-02-26 18:23:18 +0000

    format

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -3,15 +3,22 @@
 
 import argparse
 
+
 def get_args():
     parser = argparse.ArgumentParser(description="Say hello")
-    parser.add_argument('-n','--name', metavar='name', default='World', help='Name to greet')
+    parser.add_argument('-n',
+                        '--name',
+                        metavar='name',
+                        default='World',
+                        help='Name to greet')
     return parser.parse_args()
 
-def main(): 
+
+def main():
     args = get_args()
     name = args.name
     print("Hello, " + name + "!")
+
 
 if __name__ == '__main__':
     main()
