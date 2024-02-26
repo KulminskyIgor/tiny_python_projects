eba490bec773c0a8f3d0a75c891658775d1732f7
Igor Kulminskii <igor.kulminskii@telekom.com>
2024-02-26 18:02:00 +0000

    add main definition

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -3,8 +3,12 @@
 
 import argparse
 
-parser = argparse.ArgumentParser(description="Say hello")
-parser.add_argument('name',help='Name to greet')
-args = parser.parse_args()
-name = args.name
-print("Hello, " + name + "!")
+def main(): 
+    parser = argparse.ArgumentParser(description="Say hello")
+    parser.add_argument('-n','--name', metavar='name', default='World', help='Name to greet')
+    args = parser.parse_args()
+    name = args.name
+    print("Hello, " + name + "!")
+
+if __name__ == '__main__':
+    main()
